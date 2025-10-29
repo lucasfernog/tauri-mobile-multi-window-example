a7ea4cc6b53c55dfa1e29b27464f412e3185024c
Lucas Nogueira <lucas@tauri.app>
2025-10-29 17:51:15 -0300

    first commit

---
+++ src-tauri/gen/android/app/src/main/java/com/lucasfernog/multi_window_example/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lucasfernog.multi_window_example
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
